3b7b518d00aef7a1ddedbfd8868af64655a2b061
Xaugusto <150358410+Xaugusto@users.noreply.github.com>
2025-10-02 20:04:06 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
+const signInBtn = document.getElementById("signIn");
+const signUpBtn = document.getElementById("signUp");
+const firstForm = document.getElementById("form1");
+const secondForm = document.getElementById("form2");
+const container = document.querySelector(".container");
 
+signInBtn.addEventListener("click", () => {
+	container.classList.remove("right-panel-active");
+});
+
+signUpBtn.addEventListener("click", () => {
+	container.classList.add("right-panel-active");
+});
+
+// Se você não quer prevenir a submissão, remova os comentários dessas linhas
+// firstForm.addEventListener("submit", (e) => e.preventDefault());
+// secondForm.addEventListener("submit", (e) => e.preventDefault());
