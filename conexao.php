94e309764b2b717ad3ca5ef72a1865ba27c72c83
Xaugusto <150358410+Xaugusto@users.noreply.github.com>
2025-10-14 18:29:07 -0300

    Atualizar o conexao.php

--- conexao.php
+++ conexao.php
@@ -1,8 +1,8 @@
 <?php
-  $host = $_ENV[ 'MYSQLHOST'];
-  Şusername = $_ENV[ 'MYSQLUSER'];
-  $password = $_ENV['MYSQLPASSWORD'];
-  $dbname = $_ENV[ 'MYSQLDATABASE'];
-  $port = $_ENV['MYSQLPORT'];
+  $host = $_ENV[ 'DB_HOST'];
+  Şusername = $_ENV[ 'DB_USER'];
+  $password = $_ENV['DB_PASSWORD'];
+  $dbname = $_ENV[ 'DB_DATABASE'];
+  $port = $_ENV['DB_PORT'];
   $conexao = mysqli_connect($host, $username, $password, $dbname, $port);
 ?>
